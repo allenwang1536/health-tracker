2ddac42028c550ce28ee78bc01aa6715706b92b0
Allen Wang <allen_wang1@brown.edu>
2023-06-29 18:55:10 -0700

    form enter

--- src/components/Food/AddFood.jsx
+++ src/components/Food/AddFood.jsx
@@ -9,6 +9,11 @@ function AddFood({ defaultMealType }) {
     const [mealName, setMealName] = useState("");
     const [calories, setCalories] = useState();
     const [notes, setNotes] = useState("");
+    const [errors, setErrors] = useState(
+        {
+            mealName: "Enter meal name",
+            calories: "Enter calories",
+        });
 
     const openDialog = () => {
         if (dialogRef.current) {
@@ -22,15 +27,49 @@ function AddFood({ defaultMealType }) {
         }
     };
 
-    const handleSubmit = () => {
-        closeDialog();
-
-        onSubmit(mealType, mealName, calories, notes);
-
+    const resetFormValues = () => {
+        setErrors({
+            mealName: "Enter meal name",
+            calories: "Enter calories",
+        })
         setMealType(defaultMealType);
         setMealName("");
         setCalories("");
         setNotes("");
+    }
+
+    const handleExit = () => {
+        closeDialog();
+        resetFormValues();
+    }
+
+    const handleSubmit = () => {
+        const validationErrors = {};
+
+        if (!mealName) {
+            validationErrors.mealName = "Meal Name is required";
+        }
+
+        if (!calories) {
+            validationErrors.calories = "Calories are required";
+        }
+
+        if (Object.keys(validationErrors).length > 0) {
+            setErrors(validationErrors);
+            return;
+        }
+
+        closeDialog();
+
+        onSubmit(mealType, mealName, calories, notes);
+
+        resetFormValues();
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            handleSubmit();
+        }
     }
 
     return (
@@ -40,7 +79,7 @@ function AddFood({ defaultMealType }) {
                     Add New Food
                 </button>
 
-                <dialog ref={dialogRef} method="dialog" className="dialogue-box-pop-up">
+                <dialog ref={dialogRef} method="dialog" className="dialogue-box-pop-up" onKeyDown={handleKeyDown}>
                     <div className="dialogue-box-info">
                         <p> MEAL </p>
                         <label>
@@ -51,13 +90,13 @@ function AddFood({ defaultMealType }) {
                                 <option value="Snack">Snack</option>
                             </select>
                         </label>
-                        <label> Meal Name: <input value={mealName} onChange={e => setMealName(e.target.value)} /> </label>
-                        <label> Calories: <input value={calories} onChange={e => setCalories(e.target.value)} /> </label>
-                        <label> Additional Notes: <input value={notes} onChange={e => setNotes(e.target.value)} /> </label>
+                        <label> Meal Name: <input placeholder={errors.mealName} value={mealName} onChange={e => setMealName(e.target.value)} /> </label>
+                        <label> Calories: <input placeholder={errors.calories} value={calories} onChange={e => setCalories(e.target.value)} /> </label>
+                        <label> Additional Notes: <input placeholder="Enter additional notes" value={notes} onChange={e => setNotes(e.target.value)} /> </label>
 
                     </div>
                     <div className="exit-submit-container">
-                        <button onClick={closeDialog}>Exit</button>
+                        <button onClick={handleExit}>Exit</button>
                         <button onClick={handleSubmit}>Submit</button>
                     </div>
                 </dialog>
